4cfe1c97fdb9014f487bee6135047ea4b2c4747d
Navarreteed <eduardo.gutierreznavarrete@gmail.com>
2023-04-06 21:28:36 -0600

    scrapping2 new method

--- web_scrapping/web_scrapping_part2.py
+++ web_scrapping/web_scrapping_part2.py
@@ -9,7 +9,8 @@ This module only obtains data web scrapping the page, further processing is need
 import pandas as pd
 import numpy as np
 import os
-from outils import load_config, fetch_html
+import outils as ou
+
 import time
 from concurrent.futures import ThreadPoolExecutor
 
@@ -17,14 +18,14 @@ import pandas as pd
 import numpy as np
 import os
 import time
-from outils import load_config, fetch_html
+
 
 # Load config file calling load_config function
-config_f = load_config("config.yaml")
+config_f = ou.load_config("config.yaml")
 
 def get_studio_themes_genres_demographics(url):
     try:
-        soup = fetch_html(url)
+        soup = ou.fetch_html(url)
         div_elements = soup.find_all('div', class_="spaceit_pad")
 
         studio = None
@@ -54,24 +55,47 @@ def get_studio_themes_genres_demographics(url):
         return None, None, None, None
 
 def process_url(url):
-    delay = 4  # Add your desired delay in seconds here
+    delay = 1  # Add your desired delay in seconds here
     time.sleep(delay)
     return pd.Series(get_studio_themes_genres_demographics(url))
 
-top_anime = pd.read_csv(os.path.join(config_f["data_directory"]+config_f["raw_data"],"raw_anime_principal_page.csv"))
-top_anime = top_anime[101:3000]
 
-urls = top_anime['url'].tolist()
+top_anime = pd.read_csv(ou.PARENT_PATH+"/data/raw/anime_principal_page.csv")
 
-results = []
-
-total_urls = len(urls)
-
-for index, url in enumerate(urls):
-    print(f"Processing URL {index+1} of {total_urls}: {url}")
-    results.append(process_url(url))
-    print(results[-1])
-
-top_anime[['studio', 'themes', 'genres', 'demographics']] = pd.DataFrame(results)
-
-top_anime.to_csv(os.path.join(config_f["data_directory"]+config_f["raw_data"],"raw_anime_principal_and_secondary_pages.csv"),index=False)
+try:
+    current_anime = pd.read_csv(ou.PARENT_PATH+"/data/raw/anime_principal_and_secondary_pages.csv")['ranking'].max()
+    if current_anime+50 >= 13850: # To ensure there's no error when reaching final iterations
+        top_anime = top_anime[current_anime:13850]
+        urls = top_anime['url'].tolist()
+        results = []
+        total_urls = len(urls)
+        for index, url in enumerate(urls):
+            print(f"Processing URL {index+1} of {total_urls}: {url}")
+            results.append(process_url(url))
+            print(results[-1])
+        top_anime[['studio', 'themes', 'genres', 'demographics']] = pd.DataFrame(results)
+        top_anime.to_csv(ou.PARENT_PATH+"/data/raw/anime_principal_and_secondary_pages.csv",index=False, mode='a',header=True)    
+        print(current_anime)
+    else: #Appends 50 new animes each time it runs
+        top_anime = top_anime[current_anime:current_anime+50]
+        urls = top_anime['url'].tolist()
+        results = []
+        total_urls = len(urls)
+        for index, url in enumerate(urls):
+            print(f"Processing URL {index+1} of {total_urls}: {url}")
+            results.append(process_url(url))
+            print(results[-1])
+        top_anime[['studio', 'themes', 'genres', 'demographics']] = pd.DataFrame(results)
+        top_anime.to_csv(ou.PARENT_PATH+"/data/raw/anime_principal_and_secondary_pages.csv",index=False, mode='a', header=True)    
+        print(current_anime)
+except: # This line runs only when there's no file called anime_principal_and_secondary_pages.csv
+    top_anime = top_anime[0:50]
+    urls = top_anime['url'].tolist()
+    results = []
+    total_urls = len(urls)
+    for index, url in enumerate(urls):
+        print(f"Processing URL {index+1} of {total_urls}: {url}")
+        results.append(process_url(url))
+        print(results[-1])
+    top_anime[['studio', 'themes', 'genres', 'demographics']] = pd.DataFrame(results)
+    top_anime.to_csv(ou.PARENT_PATH+"/data/raw/anime_principal_and_secondary_pages.csv",index=False)
