20f416648dcac9a74e63eb7cc76e05514d49d8de
CAMM961001 <miguel.castama@gmail.com>
2023-04-16 12:48:29 -0500

    Add utils functions to file

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import yaml
 import logging
 import requests
 
+from ast import literal_eval
+
 CURRENT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -18,3 +20,30 @@ def load_config(config_name):
     with open(os.path.join(CURRENT, config_name), encoding="utf-8") as conf:
         config = yaml.safe_load(conf)
     return config
+
+
+def global_format(x):
+    '''
+    Remove atypical character from given string
+
+    Params:
+        x: String to format
+    
+    Returns:
+        x: String without atypical characters
+    '''
+    x = re.sub(pattern=r"[°.-]+", repl='_', string=x)
+    x = re.sub(pattern="'s", repl='', string=x)
+    return x
+
+# lambda function to format single string data in cell
+format_single_str = lambda cel_: '_'.join(
+    cel_.rstrip().lstrip().split(sep=' ')
+).lower()
+
+# lambda function to format list like data in cell
+format_list_str = lambda cel_: [
+    global_format(
+        '_'.join(val_.split(' ')).lower()
+    ) for val_ in literal_eval(cel_)
+]
